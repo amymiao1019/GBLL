1b64523a3560d5a21b73c35109396d2f63cdca39
amymiao1019 <ivannole1019@gmail.com>
2025-12-22 13:54:20 +1100

    Update GBLL.R

--- R code/GBLL.R
+++ R code/GBLL.R
@@ -15,3 +15,53 @@ lc <- function(data){
 }
 
 
+# Create Fourier Regressors in the forecasting process
+fourier_regressor <- function(date, train_time, horizon, K){
+  # create the train and test weekly date
+  date.new <- data.frame(date = date[1:train_time])
+  train_weekly <-  date.new%>%
+    mutate(date=as.Date(date,format='%d/%m/%Y'),
+           ddate=decimal_date(date)%%1)%>%pull(ddate)
+  test_weekly <- decimal_date(date[(train_time+1):(train_time+horizon)])%%1
+  
+  # create the fourier regressor
+  X_train <- matrix(NA, train_time, 2*K)
+  X_test <- matrix(NA, horizon, 2*K)
+  
+  for(k in 1:K){
+    X_train[,(k-1)*2+1] <- sin(2*pi*k*train_weekly)
+    X_train[,k*2] <- cos(2*pi*k*train_weekly)
+    
+    X_test[,(k-1)*2+1] <- sin(2*pi*k*test_weekly)
+    X_test[,k*2] <- cos(2*pi*k*test_weekly)
+  }
+  
+  output <- list(train=X_train, test=X_test)
+  return(output)
+}
+
+
+# Create the objective function to tune the learning rate gamma in the GBLL framework
+obj <- function(n_pop, n_age, mortality_rate, fitted_mortality, gamma){
+  obj_sum <- 0
+  if(n_pop == 1){
+    for(j in 1:n_age){
+      fitted <- fitted_mortality[,j]
+      error <- mortality_rate[,j] - gamma * fitted
+      sq_error <- t(error)%*%error
+      obj_sum <- obj_sum + sq_error
+    }
+  } else{
+    for(i in 1:n_pop){
+      for(j in 1:n_age){
+        fitted <- fitted_mortality[[i]][,j]
+        error <- mortality_rate[[i]][,j] - gamma * fitted
+        sq_error <- t(error)%*%error
+        obj_sum <- obj_sum + sq_error
+      }
+    }
+  }
+  return(obj_sum)
+}
+
+
