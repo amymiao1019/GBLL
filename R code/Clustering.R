c1c89c5a3030cb67cf41caf19432266c1060bc68
amymiao1019 <ivannole1019@gmail.com>
2025-12-22 14:31:10 +1100

    Create Clustering.R

---
+++ R code/Clustering.R
@@ -0,0 +1,55 @@
+1. Fit Lee-Carter model on each country to extract the time trend
+overall_lc_kapa_v3 <- matrix(0, nrow = 260, ncol = length(read_excel_name_v3))
+for(i in 1:length(read_excel_name_v3)){
+  overall_lc_kapa_v3[,i] <- lc(log(data_list_v3[[i]][,3:6]))$kapa
+}
+lc_kapa_ts_v3 <- ts(overall_lc_kapa_v3, frequency = 52)
+
+
+
+2. Clutsering Method 1:
+raw_lc_kapa_ts_v3 <- t(lc_kapa_ts_v3)
+raw_lc_kapa_ts_NH_v3 <- raw_lc_kapa_ts_v3[-c(1,18),]
+
+
+# Find the optimal no. of cluster
+## Elbow method
+set.seed(1019)
+wcss <- numeric(10)
+for (k in 1:10) {
+  kmeans_model <- kmeans(raw_lc_kapa_ts_NH_v3, centers = k, nstart = 25)
+  wcss[k] <- kmeans_model$tot.withinss
+}
+
+plot(1:10, wcss, type = "b", pch = 19, xlab = "Number of Clusters", 
+     ylab = "Within-cluster sum of squares", main = "Elbow Method")
+
+
+
+## Silhouette Method
+sil_width <- numeric(10)
+for (k in 2:10) {
+  kmeans_model <- kmeans(raw_lc_kapa_ts_NH_v3, centers = k, nstart = 25)
+  sil <- silhouette(kmeans_model$cluster, dist(raw_lc_kapa_ts_NH_v3))
+  sil_width[k] <- mean(sil[, 3])
+}
+
+plot(2:10, sil_width[-1], type = "b", pch = 19, xlab = "Number of Clusters", 
+     ylab = "Average Silhouette Width", main = "Silhouette Method")
+
+
+# Apply the optimal no. of cluster
+raw_lc_kapa_ts_NH_v3_clusters  <- raw_lc_kapa_ts_NH_v3  %>%
+  kmeans(centers = 2, nstart = 25)
+
+raw_lc_kapa_ts_NH_v3_clusters$cluster["Series 1"] <- 3
+raw_lc_kapa_ts_NH_v3_clusters$cluster["Series 18"] <- 3
+ordered_raw_lc_kapa_ts_NH_v3 <- raw_lc_kapa_ts_NH_v3_clusters$cluster[order(as.numeric(gsub("Series ", "", names(raw_lc_kapa_ts_NH_v3_clusters$cluster))))]
+
+# cluster result
+lapply(1:3, \(i) read_excel_name_v3[ordered_raw_lc_kapa_ts_NH_v3 == i])
+
+
+
+
+3. 
